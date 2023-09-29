405af659569495fa8858bc5de4dd81868df6c69d
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-29 15:53:04 +0300

    add 2 js files and made 3 functions

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,3 +1,20 @@
-function fetchBreeds(array) {
-  const BASE = 'https://api.thecatapi.com';
+export function fetchBreeds(catId) {
+  const BASE_URL = 'https://api.thecatapi.com';
+  const END_POINT = '/v1/images/search';
+  const PARAMS = `?breed_ids=${catId}`;
+  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
+
+  const options = {
+    headers: {
+      'x-api-key':
+        'live_VOvT6IAsVg3apVKiZYzTEFlSsXzX8nEsF1JTdvq7UHoVVXUDRSRohjOihM0jsZDU',
+    },
+  };
+
+  return fetch(url, options).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_VOvT6IAsVg3apVKiZYzTEFlSsXzX8nEsF1JTdvq7UHoVVXUDRSRohjOihM0jsZDU';
+import { fetchBreeds } from './cat-api';
+
+// axios.defaults.headers.common['x-api-key' =
+//   'live_VOvT6IAsVg3apVKiZYzTEFlSsXzX8nEsF1JTdvq7UHoVVXUDRSRohjOihM0jsZDU';
+
+const select = document.querySelector('.breed-select');
+const divCat = document.querySelector('.cat-info');
+
+select.addEventListener('change', onClickSelect);
+
+function onClickSelect() {
+  const selectedCat = select.value;
+  fetchBreeds(selectedCat).then(catInfo => {
+    renderPage(catInfo[0]);
+  });
+}
+
+function renderPage({ url, name, description, temperament }) {
+  const markup = `<img src="${url}" alt="cat image">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <p>${temperament}</p`;
+
+  divCat.innerHTML = markup;
+}
